f44b232a75e33676e8581f89bb72dec3c0eec934
v3nc1slav <v.verchov@mail.bg>
2021-01-16 11:54:57 +0200

    add two class BaseInfo and Category

---
+++ DevEndForum.Server/Data/Models/BaseInfo.cs
@@ -0,0 +1,26 @@
+﻿namespace DevEndForum.Server.Data.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public abstract class BaseInfo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public bool IsDelete { get; set; } = false;
+
+        [Required]
+        public DateTime DeleteOn { get; set; } = DateTime.UtcNow;
+
+        [Required]
+        public bool IsModifait { get; set; } = false;
+
+        [Required]
+        public DateTime ModifaitOn { get; set; } = DateTime.UtcNow;
+
+        [Required]
+        public DateTime CreateOn { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ DevEndForum.Server/Data/Models/Category.cs
@@ -0,0 +1,28 @@
+﻿namespace DevEndForum.Server.Data.Models
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Category : BaseInfo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string Img { get; set; }
+
+        [Required]
+        public int ThemesCount { get; set; } = 0;
+
+        [Required]
+        public int OpinionsCount { get; set; } = 0;
+
+        public ICollection<Theme> Themes { get; set; } = new List<Theme>();
+    }
+}
